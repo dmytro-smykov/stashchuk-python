394d7a9e589a549bd0fa6b40329fc02a4f86ca85
Dmytro Smykov <dmytrosmykov@gmail.com>
2023-04-13 15:16:24 +0300

    Списки

---
+++ 15_lists.py
@@ -0,0 +1,7 @@
+list = [1, 'два', 3.7, False, [5]]  # Задание 1
+list.pop(1)                         # Задание 2
+print(len(list))                    # Задание 3
+list.reverse()                      # Задание 4
+new_list = ['раз', 2]               # Задание 5
+list += new_list                    # Задание 6
+print(list)                         # Задание 7
